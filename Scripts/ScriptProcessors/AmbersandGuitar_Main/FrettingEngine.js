fedf7fa4902128efc1d030a65ddada6803852bdc
Boim <ibrahim98208@gmail.com>
2026-03-26 16:13:44 -0700

    Implemented fretting display

--- Scripts/ScriptProcessors/AmbersandGuitar_Main/FrettingEngine.js
+++ Scripts/ScriptProcessors/AmbersandGuitar_Main/FrettingEngine.js
@@ -1,56 +1,198 @@
+ const var NUMOFSTRINGS = Globals.NUMOFSTRINGS;
+ const var LOWESTNOTE = 52;
+ const var HIGHESTNOTE = 88;
+
+
+ 
+ namespace Stringtype
+ {
+ 
+     const var STRING1 = 0;
+     const var STRING2 = 1;
+     const var STRING3 = 2;
+     const var STRING4 = 3;
+     const var STRING5 = 4;
+     const var STRING6 = 5;
+ 
+ }
+ 
+ 
  const var string6Mute = Synth.getMidiProcessor("Container0String6Mute");
  const var string5Mute = Synth.getMidiProcessor("Container0String5Mute");
  const var string4Mute = Synth.getMidiProcessor("Container0String4Mute");
  const var string3Mute = Synth.getMidiProcessor("Container0String3Mute");
+ const var string2Mute = Synth.getMidiProcessor("Container0String2Mute");
+ const var string1Mute = Synth.getMidiProcessor("Container0String1Mute");
  
- reg isFirstNote = true;
- 
- namespace StringType
- {
-	 const var STRING1 = 0;
-	 const var STRING2 = 1;
-	 const var STRING3 = 2;
-	 const var STRING4 = 3;
-	 const var STRING5 = 4;
-	 const var STRING6 = 5;
- }
- 
- 
+ //variables to correspond with the Fretdisplay
+ Globals.stringNote1 = -1;
+ Globals.stringNote2 = -1;
+ Globals.stringNote3 = -1;
+ Globals.stringNote4 = -1;
+ Globals.stringNote5 = -1;
+ Globals.stringNote6 = -1;
+
+ /*
+     stringNotes holds the note played by each string
+    on each index. Index 0 holds note played by string 1 
+    (the high string) while index 5 holds note played by
+    string 5 (the low string)
+ */
+var stringNote = [];
+stringNote.reserve(NUMOFSTRINGS);
+for(var i = 0; i < NUMOFSTRINGS; i++){
+	stringNote.push(-1);
+}
+
+
+//ensure only one sampler plays a voice at a time
+
  inline function playString6(){
-	string6Mute.setAttribute("Bypass", false);
-	string5Mute.setAttribute("Bypass", true);
-	string4Mute.setAttribute("Bypass", true);
-	string3Mute.setAttribute("Bypass", true);
+    string6Mute.setAttribute("Bypass", false);
+    string5Mute.setAttribute("Bypass", true);
+    string4Mute.setAttribute("Bypass", true);
+    string3Mute.setAttribute("Bypass", true);
+    string2Mute.setAttribute("Bypass", true);
+    string1Mute.setAttribute("Bypass", true);
  }
- 
+
  inline function playString5(){
- 	string6Mute.setAttribute("Bypass", true);
- 	string5Mute.setAttribute("Bypass", false);
- 	string4Mute.setAttribute("Bypass", true);
- 	string3Mute.setAttribute("Bypass", true);
+     string6Mute.setAttribute("Bypass", true);
+     string5Mute.setAttribute("Bypass", false);
+     string4Mute.setAttribute("Bypass", true);
+     string3Mute.setAttribute("Bypass", true);
+		string2Mute.setAttribute("Bypass", true);
+	string1Mute.setAttribute("Bypass", true);
  }
- 
+
  inline function playString4(){
- 	string6Mute.setAttribute("Bypass", true);
- 	string5Mute.setAttribute("Bypass", true);
- 	string4Mute.setAttribute("Bypass", false);
- 	string3Mute.setAttribute("Bypass", true);
+     string6Mute.setAttribute("Bypass", true);
+     string5Mute.setAttribute("Bypass", true);
+     string4Mute.setAttribute("Bypass", false);
+     string3Mute.setAttribute("Bypass", true);
+string2Mute.setAttribute("Bypass", true);
+string1Mute.setAttribute("Bypass", true);
+ }
+
+ inline function playString3(){
+     string6Mute.setAttribute("Bypass", true);
+     string5Mute.setAttribute("Bypass", true);
+     string4Mute.setAttribute("Bypass", true);
+     string3Mute.setAttribute("Bypass", false);
+string2Mute.setAttribute("Bypass", true);
+string1Mute.setAttribute("Bypass", true);
  }
  
- inline function playString3(){
- 	string6Mute.setAttribute("Bypass", true);
- 	string5Mute.setAttribute("Bypass", true);
- 	string4Mute.setAttribute("Bypass", false);
- 	string3Mute.setAttribute("Bypass", true);
+ inline function playString2(){
+     string6Mute.setAttribute("Bypass", true);
+     string5Mute.setAttribute("Bypass", true);
+     string4Mute.setAttribute("Bypass", true);
+     string3Mute.setAttribute("Bypass", true);
+ string2Mute.setAttribute("Bypass", false);
+ string1Mute.setAttribute("Bypass", true);
  }
+ 
+ inline function playString1(){
+     string6Mute.setAttribute("Bypass", true);
+     string5Mute.setAttribute("Bypass", true);
+     string4Mute.setAttribute("Bypass", true);
+     string3Mute.setAttribute("Bypass", true);
+ string2Mute.setAttribute("Bypass", true);
+ string1Mute.setAttribute("Bypass", false);
+ }
+ 
+ 
+ inline function updateGlobals(){
+	 Globals.stringNote1 = stringNote[Stringtype.STRING1];
+	 Globals.stringNote2 = stringNote[Stringtype.STRING2];
+	 Globals.stringNote3 = stringNote[Stringtype.STRING3];
+	 Globals.stringNote4 = stringNote[Stringtype.STRING4];
+	 Globals.stringNote5 = stringNote[Stringtype.STRING5];
+	 Globals.stringNote6 = stringNote[Stringtype.STRING6];
+ }
+ 
+ 
+ 
+ inline function isEventStillPlaying(eventId)
+ {
+     for (var i = 0; i < 128; i++)
+     {
+         if (activeIds.getValue(i) == eventId)
+             return true;
+     }
+     return false;
+ }
+ 
+ 
+ 
+ 
  
  function onNoteOn()
 {
+	local notePlayed = Message.getNoteNumber();
+
+
+
+	if (stringNote[Stringtype.STRING6] == -1){
+		Console.print("playing string 6");
+		playString6();
+		stringNote[Stringtype.STRING6] = notePlayed;
+		updateGlobals();
+		return;
+	}else if (stringNote[Stringtype.STRING5] == -1){
+		Console.print("playing string 5");
+		playString5();
+		stringNote[Stringtype.STRING5] = notePlayed;
+		updateGlobals();
+		return;
+	}else if (stringNote[Stringtype.STRING4] == -1){
+		Console.print("playing string 4");
+		playString4();
+		stringNote[Stringtype.STRING4] = notePlayed;
+		updateGlobals();
+		return;
+	}else if (stringNote[Stringtype.STRING3] == -1){
+		Console.print("playing string 3");
+		playString3();
+		stringNote[Stringtype.STRING3] = notePlayed;
+		updateGlobals();
+		return;
+	}else if (stringNote[Stringtype.STRING2] == -1){
+		Console.print("playing string 2");
+		playString2();
+		stringNote[Stringtype.STRING2] = notePlayed;
+		updateGlobals();
+		return;
+	}else if (stringNote[Stringtype.STRING1] == -1){
+		Console.print("playing string 1");
+		playString1();
+		stringNote[Stringtype.STRING1] = notePlayed;
+		updateGlobals();
+		return;
+	}
+	
+
 	
 }
  function onNoteOff()
 {
+	local releasedNote = Message.getNoteNumber();
 	
+	if(stringNote[Stringtype.STRING6] == releasedNote){
+		stringNote[Stringtype.STRING6] = -1;
+	}else if(stringNote[Stringtype.STRING5] == releasedNote){
+		stringNote[Stringtype.STRING5] = -1;
+	}else if(stringNote[Stringtype.STRING4] == releasedNote){
+		stringNote[Stringtype.STRING4] = -1;
+	}else if(stringNote[Stringtype.STRING3] == releasedNote){
+		stringNote[Stringtype.STRING3] = -1;
+	}else if(stringNote[Stringtype.STRING2] == releasedNote){
+		stringNote[Stringtype.STRING2] = -1;
+	}else if(stringNote[Stringtype.STRING1] == releasedNote){
+		stringNote[Stringtype.STRING1] = -1;
+	}
+	
+	updateGlobals();
 }
  function onController()
 {
--- Scripts/ScriptProcessors/AmbersandGuitar_Main/Interface.js
+++ Scripts/ScriptProcessors/AmbersandGuitar_Main/Interface.js
@@ -1,17 +1,126 @@
-Content.makeFrontInterface(600, 600);function onNoteOn()
+ Content.makeFrontInterface(1200, 600);
+ 
+//connecting with fret markers on the UI
+
+const var NOTESPERSTRING = 22;
+Globals.NUMOFSTRINGS = 6;
+Globals.pitchBendOffset = 0;
+
+
+namespace stringType
+{
+
+    const var STRING1 = 0;
+    const var STRING2 = 1;
+    const var STRING3 = 2;
+    const var STRING4 = 3;
+    const var STRING5 = 4;
+    const var STRING6 = 5;
+
+}
+
+var fretImages = [];
+for (var str = 0; str < Globals.NUMOFSTRINGS; str++){
+	
+	var row = [];
+	
+	for (var j = 0; j < NOTESPERSTRING; j++){
+		row.push(Content.getComponent("String" + (str + 1) + "Fret" + j + "Marker"));
+	}
+	fretImages.push(row);
+}
+
+
+for (var i = 0; i < Globals.NUMOFSTRINGS; i++){
+	for (var j = 0; j <NOTESPERSTRING; j++){
+		fretImages[i][j].set("visible", false);
+	}
+}
+
+inline function hideAll()
+{
+    for (var i = 0; i < Globals.NUMOFSTRINGS; i++)
+        for (var j = 0; j < NOTESPERSTRING; j++)
+            fretImages[i][j].set("visible", false);
+}
+
+inline function hideString(stringNum){
+	for(var j = 0; j < NOTESPERSTRING; j++){
+		fretImages[stringNum][j].set("visible", false);
+	}
+}
+
+function onNoteOn()
 {
 	
+	Synth.startTimer(0.05);
+
 }
  function onNoteOff()
 {
-	
+	Synth.startTimer(0.05);
 }
- function onController()
+ //const var MAX_BEND_PIXELS = 15;
+
+function onController()
 {
+	
+	//script to move fret markers. Implement later if needed
+
+	/*
+
+	var normalized = 0;
+	var raw = 0;
+	
+	if (Message.getControllerNumber() == 128)
+	    {
+	        raw = Message.getControllerValue();  // 0 to 16383, center = 8192
+	
+	        // Normalize to -1.0 to +1.0
+	        normalized = (raw - 8192) / 8192.0;
+	
+	        // Convert to pixel offset
+	        Globals.pitchBendOffset = normalized * MAX_BEND_PIXELS;
+			Console.print(Globals.pitchBendOffset);
+
+	    }
+	    
+	    */
+	    
 	
 }
  function onTimer()
 {
+	
+
+	
+	hideAll();
+	
+	if(Globals.stringNote6 >= 52 && Globals.stringNote6 <= 73){
+		fretImages[stringType.STRING6][Globals.stringNote6 - 52].set("visible", true);
+	}
+	
+	if(Globals.stringNote5 >= 57 && Globals.stringNote5 <= 78){
+		fretImages[stringType.STRING5][Globals.stringNote5 - 57].set("visible", true);
+	}
+	
+	if(Globals.stringNote4 >= 62 && Globals.stringNote4 <= 83){
+		fretImages[stringType.STRING4][Globals.stringNote4 - 62].set("visible", true);
+	}
+	
+	if(Globals.stringNote3 >= 67 && Globals.stringNote3 <= 88){
+		fretImages[stringType.STRING3][Globals.stringNote3 - 67].set("visible", true);
+	}
+	
+	if(Globals.stringNote2 >= 71 && Globals.stringNote2 <= 92){
+		fretImages[stringType.STRING2][Globals.stringNote2 - 71].set("visible", true);
+	}
+	
+	if(Globals.stringNote1 >= 76 && Globals.stringNote1 <= 97){
+		fretImages[stringType.STRING1][Globals.stringNote1 - 76].set("visible", true);
+	}
+	
+	
 	
 }
  function onControl(number, value)
